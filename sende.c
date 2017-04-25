ec2867fbc13855aad86882843eebfb0254d41b5b
agobal <agobal@gmail.com>
2017-04-24 22:57:17 -0700

    Up to reposition the cursor

--- sende.c
+++ sende.c
@@ -7,6 +7,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -41,6 +45,40 @@ void enableRawMode() {
 		die("tcsetattr");
 }
 
+char editorReadKey() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN)
+		{
+			die("read");
+		}
+	}
+	return c;
+}
+
+/*** output ***/
+
+void editorRefreshScreen() 
+{
+	write(STDIN_FILENO, "\x1b[2J", 4);
+}
+
+/*** input ***/
+
+void editorProcessKeypress()
+{
+	char c = editorReadKey();
+
+	switch (c)
+	{
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
 /*** init ***/
 
 int main() {
@@ -48,18 +86,8 @@ int main() {
 
 	while (1)
 	{
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-			die("read");
-		if (iscntrl(c))
-		{
-			printf("%d\r\n", c);
-		}
-		else
-		{
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == 'q') break;
+		editorRefreshScreen();
+		editorProcessKeypress();
 	}
 	return 0;
 }
